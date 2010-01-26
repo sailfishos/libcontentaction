7eb6d3a0001546c01db9bad52af3ee96732580ef
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2010-01-26 17:37:45 +0200

    Cruft for testing the service fw signals.

--- t/servicemapper.py
+++ t/servicemapper.py
@@ -19,6 +19,26 @@ class ServiceMapper(dbus.service.Object):
     def serviceName(self, interface):
         return self.mapping.get(interface, '')
 
+    @dbus.service.method(dbus_interface='com.nokia.DuiServiceFwIf',
+                         in_signature='ss', out_signature='')
+    def emitServiceAvailable(self, interface, implementor):
+        self.serviceAvailable(interface, implementor)
+
+    @dbus.service.method(dbus_interface='com.nokia.DuiServiceFwIf',
+                         in_signature='s', out_signature='')
+    def emitServiceUnavailable(self, implementor):
+        self.serviceUnavailable(implementor)
+
+    @dbus.service.signal(dbus_interface='com.nokia.DuiServiceFwIf',
+                         signature='ss')
+    def serviceAvailable(self, interface, implementor):
+        print "emit service available", interface, implementor
+
+    @dbus.service.signal(dbus_interface='com.nokia.DuiServiceFwIf',
+                         signature='s')
+    def serviceUnavailable(self, implementor):
+        print "emit service unavailable", implementor
+
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 napper = ServiceMapper('/')
 gobject.MainLoop().run()
