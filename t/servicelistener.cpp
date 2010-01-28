9a6397dc93420d20195edf9f0cad7985ccb3df02
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2010-01-28 11:14:26 +0200

    Uglifying autotools-qt-building to get rid of rules with %.

--- t/servicelistener.cpp
+++ t/servicelistener.cpp
@@ -1,7 +1,25 @@
-#include "servicelistenerdummy.h"
-
+#include "contentaction.h"
 #include <QCoreApplication>
 #include <QTimer>
+#include <QObject>
+#include <QDebug>
+
+class Dummy : public QObject
+{
+    Q_OBJECT
+public slots:
+    void timeout()
+    {
+        static int n = 0;
+        qDebug() << "Trying to trigger" << (n+1);
+        // Try to trigger an action...
+        ContentAction::Action action = ContentAction::Action::defaultAction("an.image");
+        qDebug() << action.name();
+        action.trigger();
+        if (++n >= 4) QCoreApplication::exit();
+    }
+};
+
 
 int main(int argc, char** argv)
 {
@@ -20,3 +38,6 @@ int main(int argc, char** argv)
 
     return app.exec();
 }
+
+#include "servicelistener.moc"
+
