8105b1c6cf3fcd8075540a548c64779d8dcd9778
Akos PASZTORY <ext-akos.pasztory@nokia.com>
2010-02-10 17:34:47 +0200

    "compiles"

--- src/mime.cpp
+++ src/mime.cpp
@@ -21,75 +21,32 @@
 #include "contentaction.h"
 #include "internal.h"
 
+#include <stdlib.h>
+
 #include <gio/gdesktopappinfo.h>
 #include <gio/gio.h>
 #include <glib.h>
 
-#include <QSettings>
+#include <QStringList>
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
 #include <QDebug>
 #include <QFile>
 #include <QHash>
+#include <QDBusInterface>
+#include <QDBusPendingCall>
 
 namespace ContentAction {
 
 using namespace ContentAction::Internal;
 
-struct MimePrivate: public Action::DefaultPrivate {
-    MimePrivate(const QUrl& fileUri, struct _GAppInfo* app);
-    virtual ~MimePrivate();
-    virtual bool isValid() const;
-    virtual QString name() const;
-    virtual void trigger() const;
-    virtual DefaultPrivate *clone() const;
-
-    QUrl fileUri;
-    struct _GAppInfo* appInfo;
+enum LaunchType {
+    DontLaunch     = 0, ///< We have no clue.
+    ExecLaunch     = 1, ///< Use the Exec line from the .desktop file
+    DuiLaunch      = 2, ///< Use the Dui interface, and launch()
+    MimeOpenLaunch = 3, ///< Legacy, do mime_open
 };
-
-QString defaultAppForContentType(const QString& contentType);
-QStringList appsForContentType(const QString& contentType);
-
-MimePrivate::MimePrivate(const QUrl& fileUri, GAppInfo* appInfo)
-    : fileUri(fileUri), appInfo(appInfo)
-{ }
-
-MimePrivate::~MimePrivate()
-{
-    g_object_unref(appInfo);
-    appInfo = NULL;
-}
-
-bool MimePrivate::isValid() const
-{
-    return true;
-}
-
-QString MimePrivate::name() const
-{
-    const char* appName = g_app_info_get_name(appInfo);
-    return QString(appName);
-}
-
-/// Launches the application with gio.
-void MimePrivate::trigger() const
-{
-    GError* error = 0;
-    GList* uris = NULL;
-    QByteArray uri = fileUri.toEncoded();
-    uris = g_list_append(uris, uri.data());
-
-    g_app_info_launch_uris(appInfo, uris, NULL, &error);
-    if (error != NULL) {
-        LCA_WARNING << "cannot trigger: " << error->message;
-        g_error_free(error);
-    }
-    g_list_free(uris);
-}
-
-Action::DefaultPrivate *MimePrivate::clone() const
-{
-    return new MimePrivate(fileUri, g_app_info_dup(appInfo));
-}
 
 /// Returns the content type of the given file, or an empty string if it cannot
 /// be retrieved.
@@ -117,97 +74,136 @@ static QString contentTypeForFile(const QUrl& fileUri)
     return ret;
 }
 
-// Returns an Action, which when triggered launches the application described
-// by appInfo, passing fileUri as argument (either directly using GIO or via
-// D-Bus).
-static Action mimeAction(const QUrl& fileUri, GAppInfo *appInfo)
+// Returns the XDG data directories.
+static const QStringList& xdgDataDirs()
 {
-    return Action(new MimePrivate(fileUri, appInfo));
+    static bool dirsSet = false;
+    static QStringList dirs;
+
+    if (dirsSet)
+        return dirs;
+
+    dirsSet = true;
+    const char *d;
+    d = getenv("XDG_DATA_HOME");
+    if (d)
+        dirs.append(QString::fromLocal8Bit(d));
+    else
+        dirs.append(QDir::homePath() + "/.local/share/");
+    d = getenv("XDG_DATA_DIRS");
+    if (!d)
+        d = "/usr/local/share/:/usr/share/";
+    dirs.append(QString::fromLocal8Bit(d).split(":", QString::SkipEmptyParts));
+    return dirs;
 }
 
-/// Returns the default action for a given \a file, based on its content type.
-Action Action::defaultActionForFile(const QUrl& fileUri)
+// Reads "Key=Value" formatted lines from the file, and updates dict with
+// them.
+static void readKeyValues(QFile& file, QHash<QString, QString>& dict)
 {
-    QString contentType = contentTypeForFile(fileUri);
-    if (contentType.isEmpty())
-        return Action();
-    GAppInfo *appInfo = g_app_info_get_default_for_type(
-        contentType.toAscii().constData(), TRUE);
-    if (appInfo == NULL)
-        return Action();
-    return mimeAction(fileUri, appInfo);
-}
-
-/// Returns the set of applicable actions for a given \a file, based on its
-/// content type.
-QList<Action> Action::actionsForFile(const QUrl& fileUri)
-{
-    QList<Action> result;
-
-    QString contentType = contentTypeForFile(fileUri);
-    if (contentType.isEmpty())
-        return result;
-
-    GList* infoList;
-    infoList = g_app_info_get_all_for_type(contentType.toAscii().constData());
-    for (GList *cur = infoList; cur; cur = cur->next)
-        result << mimeAction(fileUri, G_APP_INFO(cur->data));
-    g_list_free(infoList);
-    return result;
-}
-
-QHash<QString, QString> readKeyValues(const QString& fileName)
-{
-    QHash<QString, QString> ret;
-    QFile file(fileName);
-    if (!file.exists()) {
-        LCA_WARNING << "not found:" << fileName;
-        return ret;
-    }
     if (!file.open(QIODevice::ReadOnly)) {
-        LCA_WARNING << "cannot read:" << fileName;
-        return ret;
+        LCA_WARNING << "cannot read" << file.fileName();
+        return;
     }
     QTextStream stream(&file);
-    // Ignore the header
-    stream.readLine();
-    int n = 1;
     while (!stream.atEnd()) {
         QString line = stream.readLine();
-        if (line.isNull()) break;
-        ++n;
-        QStringList keyAndValue = line.split("=", QString::SkipEmptyParts);
-        if (keyAndValue.size() < 2) {
-            LCA_WARNING << "ignoring a syntax error in" << fileName << "line" << n;
+        if (line.isNull())
+            break;
+        if (line.isEmpty() || line[0] == '[' || line[0] == '#')
             continue;
-        }
-        ret.insert(keyAndValue[0], keyAndValue[1]);
+        QStringList keyAndValue = line.split('=', QString::SkipEmptyParts);
+        if (keyAndValue.size() < 2)
+            continue;
+        dict.insert(keyAndValue[0], keyAndValue[1]);
     }
-    return ret;
+    file.close();
 }
 
-/// Returns the default application for handling the given \a contentType. The
-/// default application is read from the defaults.list. If there is no default
-/// application, returns an empty string.
-QString defaultAppForContentType(const QString& contentType)
+// Determines how to launch the application that the given .desktop file
+// represents.  Returns the LaunchType and fills service, interface and method
+// if applicable.
+static LaunchType launchInfo(const QString& desktopFile,
+                             QString& service,
+                             QString& iface, QString& method)
 {
-    QHash<QString, QString> apps = readKeyValues("/usr/share/applications/defaults.list");
-    // FIXME: file name
-    if (apps.contains(contentType)) return apps[contentType];
-    return "";
+    QFile file(desktopFile);
+    if (!file.exists())
+        return DontLaunch;
+    QHash<QString, QString> appInfo;
+    readKeyValues(file, appInfo);
+    if (appInfo.contains("X-Maemo-Service")) {
+        service = appInfo["X-Maemo-Service"];
+        iface = "com.nokia.DuiApplicationIf";
+        method = "launch";
+        return DuiLaunch;
+    }
+    if (appInfo.contains("X-Osso-Service")) {
+        service = appInfo["X-Osso-Service"];
+        iface = service;
+        method = "mime_open";
+        return MimeOpenLaunch;
+    }
+    return ExecLaunch;
+}
+
+// Searches XDG dirs for the .desktop file with the given id
+// ("something.desktop"), and returns the first hit.
+static QString findDesktopFile(const QString& id)
+{
+    QStringList dirs = xdgDataDirs();
+    for (int i = dirs.size(); i >= 0; --i) {
+        QFile f(dirs[i] + "/applications/" + id);
+        if (f.exists())
+            return f.fileName();
+    }
+    return QString();
+}
+
+// Returns the default application for handling the given \a contentType. The
+// default application is read from the defaults.list. If there is no default
+// application, returns an empty string.
+static QString defaultAppForContentType(const QString& contentType)
+{
+    static bool read = false;
+    static QHash<QString, QString> defaultApps;
+
+    if (!read) {
+        QStringList dirs = xdgDataDirs();
+        for (int i = dirs.size(); i >= 0; --i) {
+            QFile f(dirs[i] + "/applications/defaults.list");
+            if (!f.exists())
+                continue;
+            readKeyValues(f, defaultApps);
+        }
+    }
+    read = true;
+    return defaultApps.value(contentType, QString());
 }
 
 /// Returns the applications which handle the given \a contentType. The
 /// applications are read from the mimeinfo.cache. The file is searched in the
 /// default locations. The returned list will contain elements of the form
 /// "appname.desktop".
-QStringList appsForContentType(const QString& contentType)
+static QStringList appsForContentType(const QString& contentType)
 {
+    static bool read = false;
+    static QHash<QString, QString> mimeApps;
+
+    if (!read) {
+        QStringList dirs = xdgDataDirs();
+        for (int i = dirs.size(); i >= 0; --i) {
+            QFile f(dirs[i] + "/applications/mimeinfo.cache");
+            if (!f.exists())
+                continue;
+            readKeyValues(f, mimeApps);
+        }
+    }
+
+    read = true;
     QStringList ret;
-    // Get all apps handling this content type, FIXME file name
-    QHash<QString, QString> appData = readKeyValues("/usr/share/applications/mimeinfo.cache");
-    if (appData.contains(contentType))
-        ret = appData[contentType].split(";", QString::SkipEmptyParts);
+    if (mimeApps.contains(contentType))
+        ret = mimeApps[contentType].split(";", QString::SkipEmptyParts);
 
     // Get the default app handling this content type, insert it to the front
     // of the list
@@ -219,5 +215,115 @@ QStringList appsForContentType(const QString& contentType)
     return ret;
 }
 
+struct MimePrivate: public Action::DefaultPrivate {
+    MimePrivate(const QString& desktopFileName, const QUrl& fileUri);
+    virtual ~MimePrivate();
+    virtual bool isValid() const;
+    virtual QString name() const;
+    virtual void trigger() const;
+    virtual DefaultPrivate *clone() const;
+
+    QString desktopFileName;
+    QUrl fileUri;
+
+    GAppInfo *appInfo;
+    LaunchType kind;
+    QString service;
+    QString iface;
+    QString method;
+};
+
+MimePrivate::MimePrivate(const QString& desktopFileName, const QUrl& fileUri) :
+    desktopFileName(desktopFileName), fileUri(fileUri)
+{
+    kind = launchInfo(desktopFileName, service, iface, method);
+    appInfo = G_APP_INFO(g_desktop_app_info_new_from_filename(
+                             desktopFileName.toLocal8Bit().constData()));
+}
+
+MimePrivate::~MimePrivate()
+{
+    g_object_unref(appInfo);
+}
+
+bool MimePrivate::isValid() const
+{
+    return kind != DontLaunch;
+}
+
+QString MimePrivate::name() const
+{
+    const char *appName = g_app_info_get_name(appInfo);
+    return QString::fromUtf8(appName);
+}
+
+/// Launches the application, either via D-Bus or with GIO.
+void MimePrivate::trigger() const
+{
+    switch (kind) {
+    case DuiLaunch: // fall-through
+    case MimeOpenLaunch: {
+        QDBusInterface launcher(service, "/", iface);
+        launcher.asyncCall(method, fileUri);
+        break;
+    }
+    case ExecLaunch: {
+        GError *error = 0;
+        GList *uris = NULL;
+
+        QByteArray uri = fileUri.toEncoded();
+        uris = g_list_append(uris, uri.data());
+
+        g_app_info_launch_uris(appInfo, uris, NULL, &error);
+        if (error != NULL) {
+            LCA_WARNING << "cannot trigger: " << error->message;
+            g_error_free(error);
+        }
+        g_list_free(uris);
+        break;
+    }
+    default: break;
+    }
+}
+
+Action::DefaultPrivate *MimePrivate::clone() const
+{
+    return new MimePrivate(desktopFileName, fileUri);
+}
+
+/// Returns the default action for a given \a file, based on its content type.
+Action Action::defaultActionForFile(const QUrl& fileUri)
+{
+    QString contentType = contentTypeForFile(fileUri);
+    if (contentType.isEmpty())
+        return Action();
+    // We treat .desktop files specially: the default action (the only
+    // actually) is to launch the application it describes.
+    if (contentType == "application/x-desktop")
+        return Action(new MimePrivate(fileUri.toLocalFile(), QUrl()));
+    QString appid = defaultAppForContentType(contentType);
+    return Action(new MimePrivate(findDesktopFile(appid), fileUri));
+}
+
+/// Returns the set of applicable actions for a given \a file, based on its
+/// content type.
+QList<Action> Action::actionsForFile(const QUrl& fileUri)
+{
+    QList<Action> result;
+
+    QString contentType = contentTypeForFile(fileUri);
+    if (contentType.isEmpty())
+        return result;
+    if (contentType == "application/x-desktop")
+        return result << Action(new MimePrivate(fileUri.toLocalFile(),
+                                                QUrl()));
+
+    QStringList appIds = appsForContentType(contentType);
+    foreach (const QString& id, appIds) {
+        result << Action(new MimePrivate(findDesktopFile(id),
+                                         fileUri));
+    }
+    return result;
+}
 
 } // end namespace
