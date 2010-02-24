56da9f75ed6ede97cbbcf39aede0d1f62b5361fc
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2010-02-24 10:30:30 +0200

    Workaround for NB#158164.

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -23,6 +23,9 @@
 #include "internal.h"
 #include "service.h"
 
+// workaround for a bug in tracker.h; includes this too late
+#include <glib-object.h>
+
 #include <libtracker-client/tracker.h>
 
 #include <gconf/gconf.h>
