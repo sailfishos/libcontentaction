652d6045626fb3845ffa6aef81611449a1ecbf11
Akos PASZTORY <ext-akos.pasztory@nokia.com>
2010-03-10 12:54:47 +0200

    made unit test more robust

--- t/test-l10n.sh
+++ t/test-l10n.sh
@@ -13,12 +13,16 @@ abspath() {
     cd -P "$1" && pwd -P;
 }
 
+strstr() {
+    expr "$1" : "$2" >/dev/null;
+}
+
 export CONTENTACTION_ACTIONS=$srcdir/test-l10n-data
 export CONTENTACTION_L10N_PATH=test-l10n-data
 export XDG_DATA_HOME=$srcdir
 
 setlocale en_US
-a=`l10ntest an.image`
+a=$(l10ntest an.image 2>/dev/null)
 b='lack of imagination
 upload to album
 show in gallery'
@@ -28,14 +32,12 @@ test "x$a" = "x$b" || exit 1;
 plaintext="$(abspath $srcdir)/plaintext"
 
 a=$(lca-tool --l10n -f "file://$plaintext" 2>/dev/null)
-b='englishexec
-englishmimeopen
-lack of imagination'
-
-test "x$a" = "x$b" || exit 1;
+strstr "$a" ".*englishexec" || exit 1;
+strstr "$a" ".*englishmimeopen" || exit 1;
+strstr "$a" ".*lack of imagination" || exit 1;
 
 setlocale hu_HU
-a=`l10ntest an.image`
+a=$(l10ntest an.image 2>/dev/null)
 b='a kepzelet hianya
 feltoltes az albumba
 mutasd a galeriaban'
@@ -43,14 +45,12 @@ mutasd a galeriaban'
 test "x$a" = "x$b" || exit 1;
 
 a=$(lca-tool --l10n -f "file://$plaintext" 2>/dev/null)
-b='uberexec
-ubermimeopen
-a kepzelet hianya'
-
-test "x$a" = "x$b" || exit 1;
+strstr "$a" ".*uberexec" || exit 1;
+strstr "$a" ".*ubermimeopen" || exit 1;
+strstr "$a" ".*a kepzelet hianya" || exit 1;
 
 setlocale xx_YY
-a=`l10ntest an.image 2>/dev/null`
+a=$(l10ntest an.image 2>/dev/null)
 b='some.other.action
 upload.to.album
 com.nokia.imageviewer.show'
@@ -58,10 +58,8 @@ com.nokia.imageviewer.show'
 test "x$a" = "x$b" || exit 1;
 
 a=$(lca-tool --l10n -f "file://$plaintext" 2>/dev/null)
-b='uberexec
-ubermimeopen
-!! uberdui'
-
-test "x$a" = "x$b" || exit 1;
+strstr "$a" ".*uberexec" || exit 1;
+strstr "$a" ".*ubermimeopen" || exit 1;
+strstr "$a" ".*!! uberdui" || exit 1;
 
 exit 0;
