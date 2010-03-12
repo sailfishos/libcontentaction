cc659efa2b22fa6fe9d162b2e8e0eea71bfcda92
Jean-Luc Lamadon <jean-luc.lamadon@nokia.com>
2010-03-12 12:49:06 +0200

    Update build dependency to libtracker-client and header in tracker.cpp

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -23,10 +23,7 @@
 #include "internal.h"
 #include "service.h"
 
-// workaround for a bug in tracker.h; this is not defined
-#define G_BEGIN_DECLS extern "C" {
-
-#include <libtracker-client/tracker.h>
+#include <libtracker-client/tracker-client.h>
 
 #include <gconf/gconf.h>
 #include <gconf/gconf-client.h>
