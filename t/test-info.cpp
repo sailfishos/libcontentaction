ad7260a837b054f35ca7b587f0b2bf18be1be44b
Marius Vollmer <marius.vollmer@nokia.com>
2011-04-18 11:52:07 +0300

    Added test-action test executable, switched test-info to QtTestlib.

--- t/test-info.cpp
+++ t/test-info.cpp
@@ -1,31 +1,8 @@
 #include <QDebug>
 #include <QDir>
+#include <QtTest/QtTest>
 
 #include <contentinfo.h>
-
-#define EXPECT(expr,msg...)					\
-  expect((expr), #expr, __FILE__, __LINE__, ##msg, NULL)
-
-void
-expect (int b, const char *expr, const char *file, int line,
-        const char *fmt, ...)
-{
-  if (!b)
-    {
-      printf ("%s:%d: ", file, line);
-      if (fmt)
-	{
-	  va_list ap;
-	  va_start (ap, fmt);
-	  vprintf (fmt, ap);
-	  va_end (ap);
-	}
-      else
-	printf ("Expected %s", expr);
-      printf ("\n");
-      exit (1);
-    }
-}
 
 void
 dump_info (ContentInfo &info)
@@ -40,78 +17,77 @@ dump_info (ContentInfo &info)
     qDebug() << "invalid";
 }
 
-void
-test_mime_info ()
+class TestInfo : public QObject
 {
-  ContentInfo info = ContentInfo::forMime ("text/plain");
+  Q_OBJECT
 
-  EXPECT (info.isValid());
-  EXPECT (info.mimeType() == "text/plain");
-  EXPECT (info.typeDescription() == "plain text document");
-  // EXPECT (info.typeIcon() == "icon-m-content-file-unknown");
-}
-
-void
-test_file_info ()
-{
-  ContentInfo info = ContentInfo::forFile (QUrl::fromLocalFile(QDir::currentPath() + "/test-image.png"));
-
-  EXPECT (info.isValid());
-  EXPECT (info.mimeType() == "image/png");
-  EXPECT (info.typeDescription() == "PNG image");
-  // EXPECT (info.typeIcon() == "icon-m-content-file-unknown");
-}
-
-void
-test_tracker_info ()
-{
+private Q_SLOTS:
+  
+  void
+  test_mime_info ()
   {
-    ContentInfo info = ContentInfo::forTracker ("b.image");
+    ContentInfo info = ContentInfo::forMime ("text/plain");
     
-    EXPECT (info.isValid());
-    EXPECT (info.mimeType() == "image/png");
-    EXPECT (info.typeDescription() == "PNG image");
-    // EXPECT (info.typeIcon() == "icon-m-content-file-unknown");
+    QVERIFY (info.isValid());
+    QVERIFY (info.mimeType() == "text/plain");
+    QVERIFY (info.typeDescription() == "plain text document");
+    // QVERIFY (info.typeIcon() == "icon-m-content-file-unknown");
   }
 
+  void
+  test_file_info ()
   {
-    ContentInfo info = ContentInfo::forTracker ("a.music");
+    ContentInfo info = ContentInfo::forFile (QUrl::fromLocalFile(QDir::currentPath() + "/test-image.png"));
     
-    EXPECT (info.isValid());
-    EXPECT (info.mimeType() == "audio/mpeg");
-    EXPECT (info.typeDescription() == "MP3 audio");
-    // EXPECT (info.typeIcon() == "icon-m-content-file-unknown");
+    QVERIFY (info.isValid());
+    QVERIFY (info.mimeType() == "image/png");
+    QVERIFY (info.typeDescription() == "PNG image");
+    // QVERIFY (info.typeIcon() == "icon-m-content-file-unknown");
   }
 
+  void
+  test_tracker_info ()
   {
-    ContentInfo info = ContentInfo::forTracker ("urn:test:calendarevent");
-    
-    EXPECT (!info.isValid());
+    {
+      ContentInfo info = ContentInfo::forTracker ("b.image");
+      
+      QVERIFY (info.isValid());
+      QVERIFY (info.mimeType() == "image/png");
+      QVERIFY (info.typeDescription() == "PNG image");
+      // QVERIFY (info.typeIcon() == "icon-m-content-file-unknown");
+    }
+
+    {
+      ContentInfo info = ContentInfo::forTracker ("a.music");
+      
+      QVERIFY (info.isValid());
+      QVERIFY (info.mimeType() == "audio/mpeg");
+      QVERIFY (info.typeDescription() == "MP3 audio");
+      // QVERIFY (info.typeIcon() == "icon-m-content-file-unknown");
+    }
+
+    {
+      ContentInfo info = ContentInfo::forTracker ("urn:test:calendarevent");
+      
+      QVERIFY (!info.isValid());
+    }
   }
-}
 
-void
-test_bytes_info ()
-{
-  QFile file("./test-image.png");
-  file.open (QIODevice::ReadOnly);
-  QByteArray content = file.read (200);
+  void
+  test_bytes_info ()
+  {
+    QFile file("./test-image.png");
+    file.open (QIODevice::ReadOnly);
+    QByteArray content = file.read (200);
+    
+    ContentInfo info = ContentInfo::forData (content);
+    
+    QVERIFY (info.isValid());
+    QVERIFY (info.mimeType() == "image/png");
+    QVERIFY (info.typeDescription() == "PNG image");
+    // QVERIFY (info.typeIcon() == "icon-m-content-file-unknown");
+  }
+};
 
-  ContentInfo info = ContentInfo::forData (content);
-
-  EXPECT (info.isValid());
-  EXPECT (info.mimeType() == "image/png");
-  EXPECT (info.typeDescription() == "PNG image");
-  // EXPECT (info.typeIcon() == "icon-m-content-file-unknown");
-}
-
-int
-main (int argc, char **argv)
-{
-  test_mime_info ();
-  test_file_info ();
-  test_tracker_info ();
-  test_bytes_info ();
-
-  exit (0);
-}
+QTEST_MAIN(TestInfo);
+#include "test-info.moc"
