86b3773e73ae1a012001d5280cc221212f3d650e
James Thomas <james.thomas@codethink.co.uk>
2011-11-04 15:08:05 +0000

    Launch in meego-terminal if Terminal=true in desktop files

--- src/exec.cpp
+++ src/exec.cpp
@@ -36,9 +36,32 @@ ExecPrivate::ExecPrivate(QSharedPointer<MDesktopEntry> desktopEntry,
     : DefaultPrivate(desktopEntry, params)
 {
     g_type_init();
-    appInfo = G_APP_INFO(
-        g_desktop_app_info_new_from_filename(
-            desktopEntry->fileName().toLocal8Bit().constData()));
+    GKeyFile *keyFile;
+    GKeyFileFlags flags = G_KEY_FILE_NONE;
+    keyFile = g_key_file_new();
+    g_key_file_load_from_file(keyFile,
+                              desktopEntry->fileName().toLocal8Bit().constData(),
+                              flags,
+                              NULL);
+
+    // Since the list of terminals is hard coded in glib, check here to see if
+    // Terminal=true has been set
+    if (desktopEntry->terminal()) {
+        // Set it to false
+        g_key_file_set_boolean(keyFile, "Desktop Entry", "Terminal", false);
+        // We will just prepend meego-terminal -e before the actual command here
+        gchar* execString = g_key_file_get_string(keyFile,
+                                                  "Desktop Entry",
+                                                  "Exec",
+                                                  NULL);
+        gchar* terminalString = g_strdup_printf("meego-terminal -e %s",execString);
+        g_key_file_set_string(keyFile, "Desktop Entry", "Exec", terminalString);
+        g_free(execString);
+        g_free(terminalString);
+    }
+    appInfo = G_APP_INFO(g_desktop_app_info_new_from_keyfile(keyFile));
+    g_key_file_free(keyFile);
+
     if (appInfo == 0) {
         LCA_WARNING << "invalid desktop file" << desktopEntry->fileName();
     }
